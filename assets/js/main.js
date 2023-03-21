1d9ac8561ed3cbf53096532ca917f691dc475eef
OksanaKarpyn <93349703+OksanaKarpyn@users.noreply.github.com>
2023-03-21 15:45:53 +0100

    agg corezione

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,13 @@ Infine scrivi sulla pagina nomecognomecolorepreferito21 */
 // chiedo all'utente il suo nome
 let yourName = prompt('come ti chiami?');
 console.log(yourName);
-// chiedo al utente il sio cognome
+// chiedo al utente il suo cognome
 let surname = prompt('comè il tuo cognome?');
 console.log(surname);
 
-// il suo nome preferito
+// il suo colore preferito
 let favoriteColor = prompt('colore preferito?');
 console.log(favoriteColor);
 
  document.getElementById('info').innerHTML += 
-` Mi chiamo ${yourName} Il mio cognome è ${surname} Il colore preferito è ${favoriteColor} 21`;
+` Mi chiamo ${yourName}. Il mio cognome è ${surname}. Il colore preferito è ${favoriteColor} 21`;
