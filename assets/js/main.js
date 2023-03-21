7032ce199738fc9d80242c24a07858028f127e4f
OksanaKarpyn <93349703+OksanaKarpyn@users.noreply.github.com>
2023-03-21 15:06:49 +0100

    agg js

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21 */
+
+
+// chiedo all'utente il suo nome
+let yourName = prompt('come ti chiami?');
+console.log(yourName);
+// chiedo al utente il sio cognome
+let surname = prompt('comè il tuo cognome?');
+console.log(surname);
+
+// il suo nome preferito
+let favoriteColor = prompt('colore preferito?');
+console.log(favoriteColor);
+
+ document.getElementById('info').innerHTML += 
+` Mi chiamo ${yourName} Il mio cognome è ${surname} Il colore preferito è ${favoriteColor} 21`;
